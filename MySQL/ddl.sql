57e0ef9803cfd1d402dc8a942231dadc40c70f4b
Chandradip Shivankar <chandradipshivankar@gmail.com>
2024-03-22 10:51:54 +0530

    Update ddl.sql

--- MySQL/ddl.sql
+++ MySQL/ddl.sql
@@ -171,4 +171,29 @@ WHERE teacherId = 101;
 -- TRUNCATE TABLE tableName;
 
 
+-- crate table schema for one to one mapping of customer and address
+
+CREATE TABLE customer (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	first_name VARCHAR(255),
+	last_name VARCHAR(255),
+	age INT,
+	created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted BOOLEAN
+);
+
+CREATE TABLE address (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    city VARCHAR(255),
+    state VARCHAR(255),
+    pin_code INT,
+    customer_id INT,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted BOOLEAN,
+    FOREIGN KEY (customer_id) REFERENCES customer(id)
+);
+
+
 
